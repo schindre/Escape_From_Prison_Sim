d853ddb7eb31a1415321dc8e43a2f9b55b64b26c
Vebjørn Netland <33052097+Malfarian@users.noreply.github.com>
2018-02-06 17:36:46 +0100

    asd
    asd

---
+++ Assets/Scripts/playerDeath.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playerDeath : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		Debug.Log ("restart");
+		if (other.gameObject.tag == "mine") 
+		{
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+			Debug.Log ("restasdasdasdasdaart");
+			Destroy(gameObject);
+		}
+	}
+}
